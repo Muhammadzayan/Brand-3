f8e0a5866050e7964b70ddb3d3719975ed569bc6
Muhammad Zayan <112028797+Muhammadzayan@users.noreply.github.com>
2024-08-23 03:39:31 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="an-header">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light ">
+                <a class="navbar-brand" href="#"><img src="assets/img/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav m-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Why Trademark?</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> How It Works</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> Packages</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Testimonials</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"> FAQs</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="an-btn-wrap">
+                    <a href="javascript:;" class="an-btn dark">file my trademark</a>
+                    <a href="javascript:;" class="an-btn light">consult an attorney</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <section class="an-banner">
+        <div class="container">
+            <div class="row banner-row">
+                <div class="col-lg-6">
+                    <div class="content">
+                        <h1>Register your name,
+                            slogan or logo today.</h1>
+                        <p>Protect your brand online in just minutes with Trademark Engine. Starting at $49 + USPTO
+                            fees.
+                        </p>
+                        <ul>
+                            <li><img src="assets/img/icon-1.png"> 120,000+ trademarks filed since 2016</li>
+                            <li><img src="assets/img/icon-1.png"> 35,000+ five-star reviews</li>
+                            <li><img src="assets/img/icon-1.png"> Rated 4.8 by Forbes Advisor</li>
+                        </ul>
+                        <div class="an-btn-wrap">
+                            <a href="javascript:;" class="an-btn dark">file my trademark</a>
+                            <a href="javascript:;" class="an-btn light white">consult an attorney</a>
+                        </div>
+                        <div class="an-logos-slider">
+                            <div class="item">
+                                <img src="assets/img/logo-1.PNG" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="assets/img/logo-2.svg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="assets/img/logo-3.svg" alt="">
+                            </div>
+                            <div class="item">
+                                <img src="assets/img/logo-4.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="an-banner-form">
+                        <h3>register your trade mark now</h3>
+                        <p>Fill up your Application & Get Connect with us</p>
+                        <form method="POST" action="https://ustrademarkexperts.com/form_submit.php">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="fld-input">
+                                        <input type="text" id="name" name="name" placeholder="Your Name"
+                                            class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="fld-input">
+                                        <input type="email" id="email" name="email" placeholder="Your Email Address"
+                                            required="required" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="fld-input">
+                                        <input type="number" id="phoneNum2" name="phone" placeholder="Your phone Number"
+                                            required="required" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="fld-input">
+                                        <textarea name="message" rows="5" placeholder="Your message"
+                                            class="form-control"></textarea>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <input type="hidden" name="package" value="2742">
+                                    <input type="hidden" name="full_page_url"
+                                        value="https://ustrademarkexperts.com/<?php echo $_SERVER['REQUEST_URI'] ?? '' ?>">
+                                    <input type="hidden" name="page_url"
+                                        value="<?php echo $_SERVER['SCRIPT_URI'] ?? 'https://ustrademarkexperts.com/' ?>">
+                                    <input type="hidden" name="fid" value="E1uf6K2VEL78Jk0nxRMBIbai5x3IeTsJ1ocFfwTD">
+                                    <div class="fld-btn packageformsubmit">
+                                        <button type="submit LoginUp" class="an-btn dark">submit</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="an-process-section">
+        <div class="container">
+            <h2> <span>our Process </span> <br>
+                Trademark Registration Made Easy
+            </h2>
+            <p class="pb-5">
+                Our three-step process for registering trademarks guarantees the security of your trademark.
+            </p>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="an-process-item">
+                        <span>01</span>
+                        <h3>Form Submission</h3>
+                        <p>In the first stage, you must provide essential details about your brand. We have a brief form
+                            that takes only a few minutes to fill out.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="an-process-item cntr-item">
+                        <span>02</span>
+                        <h3>Trademark Search</h3>
+                        <p>Our team performs thorough searches to ensure that your trademark doesn’t match an existing
+                            one, minimizing the risk of legal conflicts.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="an-process-item">
+                        <span>03</span>
+                        <h3>Application Filing</h3>
+                        <p>We'll review your application to ensure consistency throughout before submitting it to the
+                            USPTO for approval for a seamless process.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   <section class="an-why-section">
+        <div class="container">
+            <h2> <span>Why trademark? </span> <br>
+            Why You Should Register a Trademark?
+            </h2>
+            <p class="pb-5">
+                Our three-step process for registering trademarks guarantees the security of your trademark.
+            </p>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="an-process-item">
+                        <span><img src="assets/img/01.png" alt=""></span>
+                        <h3>Protects from Infringements</h3>
+                        <p>Registering your trademark gives you special rights. It means only you can use your mark, and others can't copy it.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="an-process-item cntr-item">
+                        <span><img src="assets/img/02.png" alt=""></span>
+                        <h3>Trademark <br> Certified</h3>
+                        <p>By registering your trademark, you get official certification that your brand is real, proving its authenticity and making it more credible.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="an-process-item">
+                        <span><img src="assets/img/03.png" alt=""></span>
+                        <h3>Secure Your <br> Brand</h3>
+                        <p>When you register your trademark, you gain legal rights. It means you can protect your brand and maintain its integrity.</p>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="an-btn-wrap">
+                    <a href="javascript:;" class="an-btn dark">live chat</a>
+                    <a href="javascript:;" class="an-btn light">Start my application</a>
+                </div>
+        </div>
+    </section>
+
+    
+    <section class="an-faq-section">
+        <div class="container">
+            <h2> <span>Client Testimonial</span> <br>
+            Voices of Approval - Our Clients' Experiences
+            </h2>
+            <p class="pb-5">
+                Our three-step process for registering trademarks guarantees the security of your trademark.
+            </p>
+            <div class="row">
+               
+            </div>
+        </div>
+    </section>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <script>
+    $('.an-logos-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    });
+    </script>
+
+</body>
+
+</html>
